d77fdbbab4abdd3e37e64ce9ce5b3c760988795c
cam292 <cam292@pitt.edu>
2015-04-14 20:07:35 -0400

    Added methods to compare codes.

--- code.java
+++ code.java
@@ -13,4 +13,31 @@ public class code {
 	code(code c) {
 		this.colors = c.colors;
 	}
-}	
+	
+	public int sameColor(code c) {
+		int count = 0;
+		boolean[4] matched = {false,false,false,false};
+		
+		for (int i = 0; i < colors.length; i++) {
+			for (int k = 0; k < c.colors.length; k++) {
+				if (colors[i] = c.colors[k]) {
+					if (matched[k] == false) {
+						matched[k] = true;
+						count++;
+						k = c.colors.length;
+					}
+				}
+			}
+		}
+	}
+
+	public int sameColorSamePosition(code c) {
+		int count = 0;
+		
+		for (int i = 0; i < colors.length; i++) {
+			if (colors[i] = c.colors[i]) {
+				count++;
+			}
+		}
+	}
+}

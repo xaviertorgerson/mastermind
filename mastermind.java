d67b271ef1febd4840518fca39fbd8a7909cbf81
cam292 <cam292@pitt.edu>
2015-04-14 20:49:54 -0400

    Added a new method to return a color of the code.

--- mastermind.java
+++ mastermind.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.*;
 import java.awt.event.*;
 
 public class mastermind extends JFrame implements MouseListener
 {
 
-	public JLabel label;
+	
 
 	public static void main(String args[]) 
 	{
@@ -16,17 +17,15 @@ public class mastermind extends JFrame implements MouseListener
 	{
 		setSize(300, 600);
 		setTitle("Mastermind");
-
-		label = new JLabel("No Mouse Event Captured", JLabel.CENTER);
-		add(label);
-
+		getContentPane().setBackground(Color.lightGray);
+		
 		addMouseListener(this);
 		setVisible(true);
 	}
 
 	public void paint(Graphics g) {
 
-		label.setText("Drawn");
+		
 	}
 
 	//Overridden Methods from MouseListener Interface
@@ -35,26 +34,71 @@ public class mastermind extends JFrame implements MouseListener
 	/////////////////////////////////////////////////
 	public void mousePressed(MouseEvent e) 
 	{
-	   label.setText("Mouse Pressed");
+	   
 	}
 	
 	public void mouseReleased(MouseEvent e)
 	{
-	    label.setText("Mouse Released");
+	    
 	}
 	
 	public void mouseEntered(MouseEvent e) 
 	{
-	    label.setText("Mouse Entered");
+	   
 	}
 	
 	public void mouseExited(MouseEvent e) 
 	{
-	    label.setText("Mouse Exited");
+	    
 	}
 	
 	public void mouseClicked(MouseEvent e) 
 	{
-	    label.setText("Mouse Clicked");
+		
 	}
+	
+	public Ellipse2D returnGuessEllipse(int turn, int column) {
+		return new Ellipse2D.Double((column*60)+30,(turn*60)+30,50,50);	
+	}
+
+	public Ellipse2D returnResponseEllipse(int turn, int index) {
+		if (index == 0) {
+			return new Ellipse2D.Double((4*60)+15,(turn*60)+15,25,25);	
+		}
+		else if (index == 1) {
+			return new Ellipse2D.Double((4*60)+45,(turn*60)+15,25,25);	
+		}
+		else if (index == 2) {
+			return new Ellipse2D.Double((4*60)+15,(turn*60)+45,25,25);	
+		}
+		else if (index == 3) {
+			return new Ellipse2D.Double((4*60)+45,(turn*60)+45,25,25);	
+		}
+		return new Ellipse2D.Double(0,0,0,0);
+	}
+	
+	public Color returnColorForCharacter(char c){
+		if (c == 'r'){
+			return Color.red;
+		}
+		else if (c == 'g'){
+			return Color.green;
+		}
+		else if (c == 'b'){
+			return Color.blue;
+		}
+		else if (c == 'w'){
+			return Color.white;
+		}
+		else if (c == 'y'){
+			return Color.yellow;
+		}
+		else if (c == 'o'){
+			return Color.orange;
+		}
+		else{
+			return Color.lightGray;
+		}
+	}
+	
 }
